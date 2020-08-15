f05124c240d87dca6bc19ac8438bdb0dfbcac7a0
rikwebco <rikwebco@gmail.com>
2020-08-15 16:45:16 -0600

    add jshintrc

--- classes/GameData.js
+++ classes/GameData.js
@@ -1,43 +1,43 @@
 class GAMEDATA {
-    constructor(colHeads, colA, colB, colC, colD, colE){
+    constructor(colHeads, colA, colB, colC, colD, colE) {
         this.colHeads = colHeads.split('');
         this.colA = colA;
         this.colB = colB;
         this.colC = colC;
         this.colD = colD;
         this.colE = colE;
-        this.hopper = [...this.colA, ...this.colB, ...this.colC, ...this.colD, ...this.colE];   
+        this.hopper = [...this.colA, ...this.colB, ...this.colC, ...this.colD, ...this.colE];
     }
-    callList(){
+    callList() {
         return this.shuffled(this.hopper);
     }
 
-    fivePacks(){
+    fivePacks() {
         let packstock = [this.colA, this.colB, this.colC, this.colD, this.colE];
         let filepacks = [];
-        packstock.forEach(pack=>{
+        packstock.forEach(pack => {
             let tmp = this.shuffled(pack);
-            filepacks.push(tmp.slice(0,5));
+            filepacks.push(tmp.slice(0, 5));
         });
         return filepacks;
     }
-    shuffled(arr){
-        if(Array.isArray(arr)){
+    shuffled(arr) {
+        if (Array.isArray(arr)) {
             let shuffled = arr;
             let i = shuffled.length;
             let j;
             let temp;
-            while(--i > 0){
+            while (--i > 0) {
                 j = Math.floor(Math.random() * (i));
                 temp = shuffled[j];
                 shuffled[j] = shuffled[i];
                 shuffled[i] = temp;
             }
             return shuffled;
-        }else{
+        } else {
             alert('An array is required for proper function.');
         }
     }
 
-    
+
 }
